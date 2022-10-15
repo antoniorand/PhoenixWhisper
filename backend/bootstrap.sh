f10182449daed7ead7b64e7a0b3ea53bd10a2b26
antoniorand <tony.randalph@gmail.com>
2022-10-15 15:38:38 +0200

    add models

---
+++ backend/bootstrap.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+export FLASK_APP=./cashman/index.py
+
+flask --app index.py run
---
+++ backend/index.py
@@ -0,0 +1,57 @@
+from flask import Flask, jsonify, request
+
+
+from cashman.model.expense import Expense, ExpenseSchema
+from cashman.model.income import Income, IncomeSchema
+from cashman.model.transaction_type import TransactionType
+
+app = Flask(__name__)
+
+transactions = [
+    Income('Salary', 5000),
+    Income('Dividends', 200),
+    Expense('pizza', 50),
+    Expense('Rock Concert', 100)
+]
+
+
+@app.route('/incomes')
+def get_incomes():
+    schema = IncomeSchema(many=True)
+    incomes = schema.dump(
+        filter(lambda t: t.type == TransactionType.INCOME, transactions)
+    )
+    return jsonify(incomes)
+
+
+@app.route('/incomes', methods=['POST'])
+def add_income():
+    income = IncomeSchema().load(request.get_json())
+    transactions.append(income)
+    return "", 204
+
+
+@app.route('/expenses')
+def get_expenses():
+    schema = ExpenseSchema(many=True)
+    expenses = schema.dump(
+        filter(lambda t: t.type == TransactionType.EXPENSE, transactions)
+    )
+    return jsonify(expenses)
+
+
+@app.route('/expenses', methods=['POST'])
+def add_expense():
+    expense = ExpenseSchema().load(request.get_json())
+    transactions.append(expense)
+    return "", 204
+
+@app.route('/transcription', methods=['POST'])
+def add_expense():
+    expense = ExpenseSchema().load(request.get_json())
+    transactions.append(expense)
+    return "", 204
+
+
+if __name__ == "__main__":
+    app.run()
